1ca34b52855cd4ef31944d6ff19fed4995d18592
vydell <fayzaavien@gmail.com>
2024-09-12 10:09:53 +0700

    nambah modul1

--- input.java
+++ input.java
@@ -1,33 +1,43 @@
 import java.util.*; //wajib ada
+ //import
+            //import package.name.Class = import 1 kelas
+            //import java.util.Scanner; digunakan untuk mendapatkan input dari user
+            //import package.name.* = import seluruh package
+            //import java.util.*; merupakan sebuah built-in package dari JAVA
 
 public class input{
     public static void main(String[] args) {
 
-               //import
-        //import package.name.Class = import 1 kelas
-            //import java.util.Scanner; digunakan untuk mendapatkan input dari user
-        //import package.name.* = import seluruh package
-            //import java.util.*; erupakan sebuah built-in package dari JAVA
-        
-        //deklarasi dan inisialisasi variabel
+        //deklarasi variabel  inisialisasi variabel
         //[tipe data][nama] = [inisialisasi value];
-       
-        //output
         String nama1 = "Khai";
         int nilai = 90;
         float rerata = 98.78f;
         boolean mahasiswa_aktif = true;
+       
+        //proses
+        //operasi aritmatika
+        // + pertambahan
+        // - pengurangan
+        // / pembagian
+        // * perkalian
+        // % modulo (sisa pembagian)
 
+
+        //Math class
+        //perpangkatan
+        //Math.pow(a, b) a pangkat b
+        //Math.sqrt() akar 2
+        //Math.cbrt() akar 3
+        //Math.abs() nilai absolut
+        
+
+        //output
         System.out.println("Hello, World!");
         System.out.println("Praktikum "+"Pemdas");
         System.out.println("Nama Saya : "+nama1);
         System.out.println("Mahasiswa Aktif: "+mahasiswa_aktif);
 
-        //printf
-        System.out.printf("Nama saya adalah %s\n", nama1);
-        System.out.printf("Nilai saya adalah %d\n", nilai);
-        System.out.printf("Rata-rata saya adalah %.2f\n", rerata);
-        System.out.printf("Nama saya adalah %s, nilai saya %d, dan rata-rata saya %.2f\n", nama1, nilai, rerata);
 
         //input
         Scanner input = new Scanner(System.in);
@@ -39,21 +49,22 @@ public class input{
         float apaAja = input.nextFloat();
         System.out.print("Nama: "+nama+"\nNIM : "+nim+"\numur: "+umur+"\nGPA: "+gpa+"\napaAja: "+apaAja);
 
+        //printf
+        System.out.printf("Nama saya adalah %s\n", nama1);
+        System.out.printf("Nilai saya adalah %d\n", nilai);
+        System.out.printf("Rata-rata saya adalah %.2f\n", rerata);
+        System.out.printf("Nama saya adalah %s, nilai saya %d, dan rata-rata saya %.2f\n", nama1, nilai, rerata);
 
+        //%s = string
+        //%d = angka bulat
+        //%f = angka desimal
 
-        //proses
-        //operasi aritmatika
-        // + pertambahan
-        // - pengurangan
-        // / pembagian
-        // * perkalian
-        // % modulo (sisa pembagian)
+        //printf lanjut (ANGKA)
+        //%.[jumlah angka di belakang koma]f  --> membulatkan angka berkoma ke jumlah angka setelah desimal (bagaimana kalau lebih?)
+        //%d (diisi data float) --> membulatkan angka berkoma ke angka bulat terdekat
+        //%,[jumlah angka terpisah]d --> memisahkan angka bulat dengan koma berdasarkan jumlah angka yang ditentukan
+        //%0[jumlah angka yang ditampilkan]f/d --> mengisi sisa angka yang tidak ditampilkan di depan dengan angka 0
 
-        //perpangkatan
-        //Math.pow(a, b) a pangkat b
-        //Math.sqrt() akar 2
-        //Math.cbrt() akar 3
-        //Math.abs() nilai absolut
 
     }
 }
