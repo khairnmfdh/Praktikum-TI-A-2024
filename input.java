8fcb533fe870eeb2c43d2eb441b4dcf6e8eef1fa
khairnmfdh <khairnmfdh@student.ub.ac.id>
2024-09-12 08:56:51 +0700

    modul1

--- input.java
+++ input.java
@@ -1,34 +1,9 @@
-import java.util.*;
+import java.util.*; //wajib ada
 
 public class input{
     public static void main(String[] args) {
 
-        //output
-        System.out.println("Hello, World!");
-        //printf
-        String nama1 = "Khai";
-        int nilai = 90;
-        float rerata = 98.78f;
-        System.out.printf("Nama saya adalah %s\n", nama1);
-        System.out.printf("Nilai saya adalah %d\n", nilai);
-        System.out.printf("Rata-rata saya adalah %.2f\n", rerata);
-        System.out.printf("Nama saya adalah %s, nilai saya %d, dan rata-rata saya %.2f\n", nama1, nilai, rerata);
-
-        //input
-        Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan nama: ");
-        String nama = input.nextLine();
-        System.out.print("Masukkan nim: ");
-        String nim = input.nextLine();
-        System.out.print("Masukkan umur: ");
-        int umur = input.nextInt();
-        System.out.print("Masukkan gpa: ");
-        double gpa = input.nextDouble();
-        System.out.print("Masukkan apaAja: ");
-        float apaAja = input.nextFloat();
-        System.out.print("Nama: "+nama+"\nNIM : "+nim+"\numur: "+umur+"\nGPA: "+gpa+"\napaAja: "+apaAja);
-
-        //import
+               //import
         //import package.name.Class = import 1 kelas
             //import java.util.Scanner; digunakan untuk mendapatkan input dari user
         //import package.name.* = import seluruh package
@@ -36,6 +11,35 @@ public class input{
         
         //deklarasi dan inisialisasi variabel
         //[tipe data][nama] = [inisialisasi value];
+       
+        //output
+        String nama1 = "Khai";
+        int nilai = 90;
+        float rerata = 98.78f;
+        boolean mahasiswa_aktif = true;
+
+        System.out.println("Hello, World!");
+        System.out.println("Praktikum "+"Pemdas");
+        System.out.println("Nama Saya : "+nama1);
+        System.out.println("Mahasiswa Aktif: "+mahasiswa_aktif);
+
+        //printf
+        System.out.printf("Nama saya adalah %s\n", nama1);
+        System.out.printf("Nilai saya adalah %d\n", nilai);
+        System.out.printf("Rata-rata saya adalah %.2f\n", rerata);
+        System.out.printf("Nama saya adalah %s, nilai saya %d, dan rata-rata saya %.2f\n", nama1, nilai, rerata);
+
+        //input
+        Scanner input = new Scanner(System.in);
+        String nama = input.nextLine();
+        int umur = input.nextInt();
+        input.nextLine();
+        String nim = input.nextLine();
+        double gpa = input.nextDouble();
+        float apaAja = input.nextFloat();
+        System.out.print("Nama: "+nama+"\nNIM : "+nim+"\numur: "+umur+"\nGPA: "+gpa+"\napaAja: "+apaAja);
+
+
 
         //proses
         //operasi aritmatika
